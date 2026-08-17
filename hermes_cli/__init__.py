fc42f4c4d44ba3c6de5584b963856eb75a619189
黄建庭 <huangjianting@360shuke.com>
2026-08-17 21:17:51 +0800

    chore: release Core 0.20.17

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.20.16"
-__release_date__ = "2026.8.14.2"
+__version__ = "0.20.17"
+__release_date__ = "2026.8.17.1"
 
 
 def _ensure_utf8():
